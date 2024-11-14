4b9402b29c89e24c08b735e5dc9806468d8aa5b3
Max Hörmann <max.hoermann03@gmail.com>
2024-11-14 10:47:58 +0100

    finished website concept

--- src/compontents/Contact.tsx
+++ src/compontents/Contact.tsx
@@ -1,10 +1,14 @@
 import { CONTACT } from "../constants";
-
+import {motion} from "framer-motion"
 function Contact(){
     return (
         <>
             <div className="border-b border-neutral-900 pb-20">
-                <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
+                <motion.h1 
+                whileInView={{opacity: 1, y: 0}}
+                initial={{opacity: 0, y: -100}}
+                transition={{duration: 0.5}}  
+                className="my-10 text-center text-4xl">Get in Touch</motion.h1>
                 <div className="text-center tracking-tighter">
                     <a href="#" className="border-b">{CONTACT.email}</a>
                 </div>
--- src/compontents/Technologies.tsx
+++ src/compontents/Technologies.tsx
@@ -1,23 +1,65 @@
 import icon from '../assets/react.svg'
+import {motion, Variants} from "framer-motion"
+
+
+function iconVariants(duration: number){
+    return {
+        initial: {y: -10},
+        animate: {
+            y: [10, -10],
+            transition: {
+                duration: duration,
+                ease: "linear",
+                repeat: Infinity,
+                repeatType: "reverse"
+            }
+        }
+    }
+}
+
 function Technologies(){
     return (
         <>
             <div className="border-b border-neutral-800 pb-24">
-                <h1 className="my-20 text-center text-4xl">Technologies</h1>
-                <div className="flex flex-wrap items-center justify-center gap-4">
-                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                <motion.h1 
+                 whileInView={{opacity: 1, y: 0}}
+                 initial={{opacity: 0, y: -100}}
+                 transition={{duration: 1.5}}
+                className="my-20 text-center text-4xl">Technologies</motion.h1>
+                <motion.div 
+                whileInView={{opacity: 1, x: 0}}
+                initial={{opacity: 0, x: -100}}
+                transition={{duration: 1.5}}
+                className="flex flex-wrap items-center justify-center gap-4">
+                    <motion.div 
+                    variants={iconVariants(2.5) as Variants}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                         <img className="text-7xl text-cyan-400" src={icon} alt="icon" />
-                    </div>
-                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                    </motion.div>
+                    <motion.div 
+                    variants={iconVariants(5) as Variants}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                         <img className="text-7xl text-cyan-400" src={icon} alt="icon" />
-                    </div>
-                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                    </motion.div>
+                    <motion.div 
+                    variants={iconVariants(2) as Variants}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                         <img className="text-7xl text-cyan-400" src={icon} alt="icon" />
-                    </div>
-                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                    </motion.div>
+                    <motion.div
+                     variants={iconVariants(6) as Variants}
+                     initial="initial"
+                     animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                         <img className="text-7xl text-cyan-400" src={icon} alt="icon" />
-                    </div>
-                </div>
+                    </motion.div>
+                </motion.div>
             </div>
         </>
 
